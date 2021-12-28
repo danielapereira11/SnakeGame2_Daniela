1e6dc5f4d53321caa99244d28bfa3c52d7f6327a
danielapereira11 <86188533+danielapereira11@users.noreply.github.com>
2021-12-28 21:40:45 +0000

    removed temporary snake

--- index.js
+++ index.js
@@ -2,18 +2,11 @@ const mainText = document.getElementById("main-text");
 const grid = document.getElementById("grid");
 const scoreBoard = document.getElementById("score");
 const startBtn = document.getElementById("btn-start");
-
+let snake = [2, 1, 0];
+let squares = [];
 let score = 0;
 scoreBoard.innerHTML = score;
 
-let snake = [2, 1, 0];
-let gameSquare = [];
-
-function addSnake() {
   let i;
-  for (i = 0; i < snake.length; i++) {
-    grid.innerHTML += `<div class="square snake"></div>`;
   }
 }
-
-addSnake();
