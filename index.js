1127fe071f9d2c029bfceca2eedc84efa789aeec
danielapereira11 <86188533+danielapereira11@users.noreply.github.com>
2021-12-28 20:37:55 +0000

    Declaration of constants

---
+++ index.js
@@ -0,0 +1,4 @@
+const mainText = document.getElementById("main-text");
+const gameBoard = document.getElementById("main-game");
+const scoreBoard = document.getElementById("score");
+const startBtn = document.getElementById("btn-start");
